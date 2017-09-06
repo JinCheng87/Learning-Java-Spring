347622a5884f5c5751fb6f35232939bf97296e50
Jin Cheng <jincheng88@outlook.com>
2017-09-05 21:15:24 -0500

    create a service object

---
+++ src/main/java/io/javaproject/learningspring/topic/TopicService.java
@@ -0,0 +1,20 @@
+package io.javaproject.learningspring.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	private List<Topic> topics = Arrays.asList(
+			new Topic("spring", "spring framework", "spring framework description"),	
+			new Topic("java", "head first java", "head first description"),	
+			new Topic("spring", "spring action", "spring action description")				
+				);
+	
+	public List<Topic> getAllTopics(){
+		return topics;
+	}
+}
