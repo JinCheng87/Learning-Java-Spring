134e7a83b35624b85423ed49347a5ec1d1047b3f
Jin Cheng <jincheng88@outlook.com>
2017-09-05 19:16:04 -0500

    creaed mavan project and main class

---
+++ src/main/java/io/javaproject/learningspring/FirstTryApp.java
@@ -0,0 +1,13 @@
+package io.javaproject.learningspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstTryApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstTryApp.class, args);
+
+	}
+}
